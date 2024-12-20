4c74edd59878439e9a4855ef31c99ac5fb69c4b1
yurimjin <jayjinjay@gmail.com>
2024-12-20 14:34:30 +0900

    giscus init

---
+++ .vitepress/theme/components/comments.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { useData } from 'vitepress'
+
+const { title } = useData()
+</script>
+
+<template>
+  <div :key="title" class="giscus">
+    <component
+      :is="'script'"
+      src="https://giscus.app/client.js"
+      data-repo="milooy/gitcuss-test"
+      data-repo-id="R_kgDONgF71A"
+      data-category="Announcements"
+      data-category-id="DIC_kwDONgF71M4ClYlK"
+      data-mapping="pathname"
+      data-strict="0"
+      data-reactions-enabled="1"
+      data-emit-metadata="0"
+      data-input-position="bottom"
+      data-theme="preferred_color_scheme"
+      data-lang="ko"
+      crossorigin="anonymous"
+      data-loading="lazy"
+      async
+    />
+  </div>
+</template>
